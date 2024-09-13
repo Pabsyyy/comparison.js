623d4af27561291508c34229b12010ddb8fc8d13
Pabsyyy <christiangalinato1218@gmail.com>
2024-09-13 17:59:29 +0800

    initial commit

---
+++ comparison.js
@@ -0,0 +1,25 @@
+let firstfavNumber = 12;
+let secondfavNumber = 18;
+
+let greater = firstfavNumber > secondfavNumber;
+console.log(greater);
+let lessThan = firstfavNumber < secondfavNumber;
+console.log(lessThan);
+let greaterEqual = firstfavNumber >= secondfavNumber;
+console.log(greaterEqual);
+let lessEqual = firstfavNumber <= secondfavNumber;
+console.log(lessEqual);
+let strictEqual = firstfavNumber === secondfavNumber;
+console.log(strictEqual);
+let looseEquality = firstfavNumber == secondfavNumber;
+console.log(looseEquality);
+let strictInequality = firstfavNumber !== secondfavNumber;
+console.log(strictInequality);
+let looseInequality = firstfavNumber != firstfavNumber;
+console.log(looseInequality);
+let and = firstfavNumber > 0 && secondfavNumber > 0;
+console.log(and);
+let or = firstfavNumber < 0 || secondfavNumber > 0;
+console.log(or);
+let not = !(firstfavNumber == secondfavNumber);
+console.log(not);
